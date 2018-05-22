cb44c9a56ecbc203664d641cca60552524866c5c
suzukishintaro0617 <s.suzuki@atomitech.jp>
2018-05-22 11:48:08 +0900

    P28解答

--- P28/main.py
+++ P28/main.py
@@ -1,26 +1,23 @@
 def lsort(x):
-    x.sort(key=len)
+    x.sort(key = len)
     return x
 
 def lfsort(x):
-    ret=[]
+    ret = []
     for i in x:
-        if(ret==[]):
+        if ret == []:
             ret.append([i])
-            print(ret)
         else:
             is_added = False
             for j in ret:
-                if len(j[-1])==len(i):
+                if len(j[-1]) == len(i):
                     j.append(i)
-                    print(ret)
                     is_added = True
             if is_added == False:
                 ret.append([i])
-                print(ret)
-    ret.sort(key=len)
+    ret.sort(key = len)
 
-    retA=[]
+    flat = []
     for k in ret:
-        retA = retA + k
-    return(retA)
+        flat = flat + k
+    return(flat)
