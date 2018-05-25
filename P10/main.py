7fc25aba01f4e77f46dd06dcbfdccde9ef4dc0f8
suzukishintaro0617 <s.suzuki@atomitech.jp>
2018-05-25 11:23:40 +0900

    P10解答

--- P10/main.py
+++ P10/main.py
@@ -1,11 +1,13 @@
 def encode(x):
     ret_a=[]
-    for X in x:
-        if(ret_a==[] or ret_a[-1][-1]!=X):
-            ret_a=ret_a+[[X]]
+    for i in x:
+        if ( ret_a == [] or ret_a[-1][-1] != i ):
+            ret_a = ret_a + [[i]]
         else:
-            ret_a[-1].append(X)
-        ret_b=[]
-        for Y in ret_a:
-            ret_b=ret_b+[len(Y),Y[0]]
+            ret_a[-1].append(i)
+
+        ret_b = []
+        for j in ret_a:
+            ret_b = ret_b + [[len(j),j[0]]]
+
     return ret_b
